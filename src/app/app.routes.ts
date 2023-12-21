e4e6f8deec91ad7aa1d04b768996d22d7831c4fc
laska-adief <laskaadief.la@gmail.com>
2023-12-22 06:32:54 +0700

    [Dashboard Angular Standalone] config route to dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,14 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('@dashboard-angular-standalone/dashboard').then(
+        (m) => m.DashboardComponent
+      ),
+  },
+  {
     path: 'users',
     loadComponent: () =>
       import('@dashboard-angular-standalone/users').then(
