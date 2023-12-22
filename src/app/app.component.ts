6e5ded2fdf1cc5c36834045aee5ce669977fcb2f
laska-adief <laskaadief.la@gmail.com>
2023-12-22 07:25:30 +0700

    [Dashboard Angular Standalone] add footer component into app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, FooterComponent],
   selector: 'dashboard-angular-standalone-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
