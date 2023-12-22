0af1eb962d81fe5b07e964db67b46be931c11c60
laska-adief <laskaadief.la@gmail.com>
2023-12-22 07:48:10 +0700

    [Dashboard Angular Standalone] add user card component in dashboard component

--- modules/dashboard/src/lib/dashboard/dashboard.component.ts
+++ modules/dashboard/src/lib/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserComponent } from './components/user.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'dashboard-angular-standalone-dashboard',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
+  imports: [CommonModule, UserComponent, RouterModule],
 })
 export class DashboardComponent {}
